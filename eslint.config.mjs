b72559452564873f6581eb0d7af001a044d67926
detourguru <detourguru@gmail.com>
2026-08-23 23:40:28 +0900

    chore: eslint-plugin-import, prettier 설정 도입

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,42 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          alphabetize: { caseInsensitive: true, order: "asc" },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              group: "internal",
+              pattern: "@/**",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin", "external"],
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
