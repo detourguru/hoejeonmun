cd9221f97dc9842655069458a56ca06a98b262c7
detourguru <detourguru@gmail.com>
2026-08-28 01:05:38 +0900

    feat: 로딩 상태 없던 라우트에 loading.tsx 추가

---
+++ src/components/ui/route-loading.tsx
@@ -0,0 +1,7 @@
+import { LoadingGhost } from "@/components/ui/loading-ghost";
+
+export const RouteLoading = () => (
+  <div className="flex flex-1 items-center justify-center">
+    <LoadingGhost />
+  </div>
+);
