43b80e719783f973072775b02292d076704f3393
detourguru <detourguru@gmail.com>
2026-09-15 17:22:39 +0900

    chore: 공지 업데이트

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -11,7 +11,12 @@ export const SITE_DESCRIPTION =
 
 export const GA_MEASUREMENT_ID = "G-4LSMECET6C";
 
-export const CURRENT_UPDATE_ID = "2026-09-09";
-export const UPDATE_NOTICE_MESSAGE = `🎉 [업데이트 안내 (9/9)]
-- 내 공연 캘린더에서 시간이 겹치는 회차 자동 감지 및 표시
-- 캘린더에 '겹치는 일정만' 필터 추가`;
+export const CURRENT_UPDATE_ID = "2026-09-15";
+export const UPDATE_NOTICE_MESSAGE = `🎉 [업데이트 안내 (9/15)]
+- 캐스팅보드에서 배역란 추가/삭제 가능
+- 공연 상세 페이지에서 오늘 회차 시간 안내 및 캘린더 바로가기
+- 배역 표시 순서 고정
+- 겹치는 회차 표시를 테두리 방식으로 변경
+- 내 공연 겹침 판정에 무관한 공연이 포함되던 문제 수정
+- 오늘 탭에서 정보 조회 실패한 공연이 대학로/대극장 필터에서 숨겨지던 문제 수정
+- 검색창 사용 중 뒤로가기 시 페이지가 이동되던 문제 수정`;
